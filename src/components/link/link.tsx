c7b930e68686ad4949d340ca91de102bcd56049c
akira <akira@Mac-mini-2023-M2.local>
2024-02-07 14:47:05 +0900

    03: add common components

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,6 +1,47 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-export type LinkProps = {};
+import { Button } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { ReactNode } from 'react';
+export type LinkProps = {
+  href: string;
+  children: ReactNode;
+  variant?: keyof typeof variants;
+  icon?: JSX.Element;
+  shallow?: boolean;
+};
+const variants = {
+  link: {
+    variant: 'link',
+    color: 'primary',
+  },
+  solid: {
+    variant: 'solid',
+    bg: 'primary',
+    color: 'primaryAccent',
+    _hover: { opacity: '0.9' },
+  },
+  outline: {
+    valiant: 'outline',
+    color: 'primary',
+    bg: 'white',
+  },
+};
 
-export const Link = () => {
-  return <>Link</>;
+export const Link = ({
+  href,
+  children,
+  variant = 'link',
+  icon,
+  shallow = false,
+}: LinkProps) => {
+  return (
+    <NextLink shallow={shallow} href={href} passHref>
+      <Button
+        leftIcon={icon}
+        as="a"
+        {...variants[variant]}
+      >
+        {children}
+      </Button>
+    </NextLink>
+  );
 };
